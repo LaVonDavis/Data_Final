4ac40c66e77a32df6cc1c51db8eea5b223d7e168
LaVonDavis <153324880+LaVonDavis@users.noreply.github.com>
2023-12-08 22:18:21 -0500

    Update (3)Histogram.py

--- (3)Histogram.py
+++ (3)Histogram.py
@@ -5,5 +5,5 @@ pio.renderers.default='browser'
 
 df = pd.read_csv('cost_of_living_us.csv')
 df.dropna()
-fig = px.histogram(df, x='state', y='transportation_cost', color='state')
+fig = px.bar(df, x='state', y='transportation_cost', color='state')
 fig.show()
